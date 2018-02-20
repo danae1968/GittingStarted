fc1d52af6bec4393278fba0f83ac29be1e9e87cc
Danae <danaepapadopetraki@gmail.com>
2018-02-20 16:56:39 +0100

    changed back

--- HelloWorld.m
+++ HelloWorld.m
@@ -1,4 +1,4 @@
-name='Dorothy'; %%enter your name
-date='24 February 2018'; %%enter the correct date
+name='Danae'; %%enter your name
+date='20 February 2018'; %%enter the correct date
 
 fprintf('Hello World. I am %s and the date is %s',name,date);
