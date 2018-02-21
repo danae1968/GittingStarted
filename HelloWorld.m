1139ebf061af94b085361e5e5b44b07b63ba7258
Danae <danaepapadopetraki@gmail.com>
2018-02-21 10:44:38 +0100

    made date 31 february

--- HelloWorld.m
+++ HelloWorld.m
@@ -1,4 +1,4 @@
 name='Danae'; %%enter your name
-date='20 February 2018'; %%enter the correct date
+date='31 February 2018'; %%enter the correct date
 
 fprintf('Hello World. I am %s and the date is %s',name,date);
