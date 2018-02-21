3cc7219f39f58b3cd0cf33c046791de57b308daf
Danae <danaepapadopetraki@gmail.com>
2018-02-21 10:46:27 +0100

    changed the month to march

--- HelloWorld.m
+++ HelloWorld.m
@@ -1,4 +1,4 @@
 name='Danae'; %%enter your name
-date='31 February 2018'; %%enter the correct date
+date='31 March 2018'; %%enter the correct date
 
 fprintf('Hello World. I am %s and the date is %s',name,date);
